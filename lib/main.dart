8c09b7da8cba43d75de21b487d4443eb70e8fe19
Bellsam Beltaief <balsembeltaief90@gmail.com>
2024-06-12 14:20:36 +0100

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:e_shop/providers/p_products.dart';
 import 'package:e_shop/widgets/home.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  /// Connexion au Firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
@@ -16,6 +17,30 @@ void main() async {
       storageBucket: "e-shop-7a2f1.appspot.com",
     ),
   );
+
+  // Initialiser Firebase Messaging et request permission
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+    provisional: false,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    if (kDebugMode) {
+      print('User granted permission');
+    }
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    if (kDebugMode) {
+      print('User granted provisional permission');
+    }
+  } else {
+    if (kDebugMode) {
+      print('User declined or has not accepted permission');
+    }
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => PProducts(),
